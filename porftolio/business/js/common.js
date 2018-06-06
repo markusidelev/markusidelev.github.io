746d4618917bb196bbb9851c6c7edbb85bc52cff
markusidelev <markusidelev@gmail.com>
2018-06-06 14:59:23 +0300

    Added Business to portfolio

---
+++ porftolio/business/js/common.js
@@ -0,0 +1,16 @@
+topbar.hide()
+
+
+$("nav a").click(function(e)	{
+	e.preventDefault();
+	var href=$(this).attr("href");
+	$("html,body").animate({scrollTop:$(href).offset().top},1000);
+});
+
+
+
+$(".banner a").click(function(e)	{
+	e.preventDefault();
+	var href=$(this).attr("href");
+	$("html,body").animate({scrollTop:$(href).offset().top},1000);
+});
