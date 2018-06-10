c42dbe0ad6098edfbefad8268d46d282a434b221
markusidelev <markusidelev@gmail.com>
2018-06-10 19:57:37 +0300

    Added Swiper to Ttrex

--- porftolio/ttrex/js/common.js
+++ porftolio/ttrex/js/common.js
@@ -15,3 +15,37 @@ $("nav a").not(":first").click(function(e)	{
 $(".animated").hover(function(){
 	$(".animated").toggleClass("rubberBand infinite");
 });
+
+var mySwiper = new Swiper ('.team .team-block .swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: '3',
+    // centeredSlides: true,
+    spaceBetween: 20,
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+    	el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+    	nextEl: '.swiper-button-next',
+    	prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+    	el: '.swiper-scrollbar',
+    },
+    breakpoints:{
+    	1024: {
+
+    		slidesPerView: '2',
+    	},
+    	700:{
+    		slidesPerView: '1',
+    	}, 
+    }
+})
