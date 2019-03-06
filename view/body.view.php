de084da7c1f3f46deed435939f8e09910e4d2409
markusidelev <31567629+markusidelev@users.noreply.github.com>
2019-03-06 14:18:06 +0200

    -index page is now php. added view folder

---
+++ view/body.view.php
@@ -0,0 +1,1 @@
+<!-- <?php include __DIR__ . "/header.view.php" ?> -->
--- view/index.view.php
+++ view/index.view.php
@@ -1,12 +1,27 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>markusidelev</title>
-	<link rel="stylesheet" href="css/style.css">
+    <title>markusidelev</title>
+    <meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'/>
+    <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
-	<h1>markusidelev</h1>
+
+<?php include __DIR__ . '/body.view.php' ?>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
+
+<script src="/assets/js/common.js"></script>
+
+
+
+
+	<!-- <h1>markusidelev</h1>
 	<h2>Caйт-портфолио</h2>
 
 	<br>
@@ -26,6 +41,7 @@
 	<br>
 	<br>
 	<h3>Сайт в разработке</h3>
-	<img src="Ork_In_Progress.png" alt="">
+	<img src="Ork_In_Progress.png" alt=""> -->
+
 </body>
 </html>
