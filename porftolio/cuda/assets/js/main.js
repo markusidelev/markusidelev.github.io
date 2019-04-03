56adb8e410086825976ea2bc90d83c031778dfc6
markusidelev <31567629+markusidelev@users.noreply.github.com>
2019-04-03 17:29:09 +0300

    -Cuda site added in portfolio

---
+++ porftolio/cuda/assets/js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function (){
+	$('span.nav-menu_btn').click(function(){
+		if($('nav').hasClass('open')){
+			$('nav').removeClass('open');
+		} else{
+			$('nav').toggleClass('open');
+		}
+	})
+});
+
+function changeToCross(x) {
+	x.classList.toggle("change");
+};
+
+
+$("nav a").not(":first").click(function(e)	{
+	e.preventDefault();
+	$('nav').removeClass('open');
+	$('.nav-menu_btn').removeClass('change');
+	var href=$(this).attr("href");
+	$("html,body").animate({scrollTop:$(href).offset().top},1000);
+});
+
+$("nav a").click(function(e)	{
+	e.preventDefault();
+	var href=$(this).attr("href");
+	$("html,body").animate({scrollTop:$(href).offset().top},1000);
+});
+
+$(".big_btn").click(function(e)	{
+	e.preventDefault();
+	var href=$(this).attr("href");
+	$("html,body").animate({scrollTop:$(href).offset().top},1000);
+});
+
+$(".animated").hover(function(){
+	$(".animated").toggleClass("rubberBand infinite");
+});
