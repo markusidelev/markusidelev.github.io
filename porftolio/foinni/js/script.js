32d4dda11a8d6ef147aa829b4c27c392a86c4bed
markusidelev <markusidelev@gmail.com>
2022-01-24 15:30:37 +0200

    portfolio sites transfer

--- porftolio/foinni/js/script.js
+++
@@ -1,7 +0,0 @@
-$(".works-block a").fancybox();
-
-		$(".top-block a").click(function(e){
-			e.preventDefault();
-		var href=$(this).attr("href");   //#services
-		$("html,body").animate({scrollTop:$(href).offset().top},1000);
-	});
