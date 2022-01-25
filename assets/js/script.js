a0f2be20faeac8b04c270640d9e1a4e08f7e95bb
markusidelev <markusidelev@gmail.com>
2022-01-25 12:39:25 +0200

    js darkmode
    darkmode button added
    darkmode js toggle body class function implemented

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector('.darkmode_button');
+const body= document.body
+
+btn.addEventListener('click', function() {
+  
+    // if (condition) {
+        
+    // }
+
+  body.classList.toggle('darkmode');  
+})
+
+
+// $('#darkmode_button').on('click', function(e) {
+    
+// })
