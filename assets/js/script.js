8be0a95fb494246f1d5a377bd807b2a4f9a99f2e
markusidelev <markusidelev@gmail.com>
2022-01-25 18:24:37 +0200

    darkmode js functionality
    added darkmode auto set based on os mode preferences
    added automode button
    added darkmode save to local memory function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,67 @@
-const btn = document.querySelector('.darkmode_button');
-const body= document.body
+"use strict"
+
+/* ============================= Darkmode ======================= */
+const btn = document.querySelector('.darkmode_button')
+const autoBtn = document.querySelector('.automode_button')
+
+const body = document.body
+const currentTheme = localStorage.getItem('styleTheme')
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)")
+
+
+let saveTheme = (theme) => localStorage.setItem('styleTheme', theme)
+
+let turnDark = (theme) => {
+  body.classList.add('darkmode')
+  saveTheme(theme)
+}
+
+let turnLight = (theme) => {
+  body.classList.remove('darkmode')
+  saveTheme(theme)
+}
+
+let turnAuto = () => {
+  body.classList.remove('darkmode')
+  saveTheme('automode')
+  checkAuto()
+}
+
+
+let checkAuto = () => {
+
+    if (prefersDarkScheme.matches) {
+      turnDark('automode')
+    } else {
+      turnLight('automode')
+    }
+}
+
+
+let checkLocalTheme = () => {
+    if (currentTheme == 'darkmode') {
+      turnDark('darkmode')
+    } else if (currentTheme == 'lightmode') {
+      turnLight('lightmode')
+    } else if (currentTheme == 'automode') {
+      turnAuto()
+    } else {
+      turnAuto()
+    }
+}
+
+checkLocalTheme()
 
 btn.addEventListener('click', function() {
-  
-    // if (condition) {
-        
-    // }
-
-  body.classList.toggle('darkmode');  
+  if (body.classList.contains('darkmode')) {
+    turnLight('lightmode')
+   
+  } else {
+    turnDark('darkmode')
+    
+  }
 })
 
-
-// $('#darkmode_button').on('click', function(e) {
-    
-// })
+autoBtn.addEventListener('click', function() {
+  turnAuto()
+})
